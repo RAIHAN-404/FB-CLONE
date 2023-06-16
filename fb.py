c6dee253e05a19758e2bfe64340c55297f52d9c4
RAIHAN-404 <134984963+RAIHAN-404@users.noreply.github.com>
2023-06-16 22:39:38 +0600

    Update fb.py

--- fb.py
+++ fb.py
@@ -162,7 +162,7 @@ def RAIHAN(uid,pwx,tl):
             "pass":ps,
             "login":"Log In"}
             #_____Mathoid______#
-            authority': 'p.facebook.com',
+            authority': 'mbasic.facebook.com',
      "method": 'GET',
     "scheme": 'https',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
